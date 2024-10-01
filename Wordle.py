162b6795e233707af3172e3eda7b035fb2f6feae
fksheets <fksheets@willamette.edu>
2024-09-30 21:07:13 -0700

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -19,11 +19,22 @@ def wordle():
         if guess.lower() in ENGLISH_WORDS:
             gw.show_message('Good Job!')
             color_row(row,answer,guess)
+            
         else:
             gw.show_message('not word in list')
 
     def word_to_row(row:int, word:str):
         gw.show_message("words")
+
+    def set_row(guess):
+        row = gw.get_current_row()
+        if answer == guess:
+            gw.show_message("way to go buddy!")
+            gw.set_current_row(N_ROWS)
+        elif row == N_ROWS - 1:
+            gw.show_message("you lost, the answer was " + answer)
+        else:
+            gw.set_current_row(row+1)
 
     def word_from_row(row:int) -> str:
         string = gw.get_square_letter(row,0) + gw.get_square_letter(row,1) + gw.get_square_letter(row,2) + gw.get_square_letter(row,3) + gw.get_square_letter(row,4)
@@ -33,12 +44,12 @@ def wordle():
     def color_row(row:int,answer:str,guess:str):
         guess = word_from_row(row).lower()
         unmatched = answer
-        #unmatched = guess
+        
         print(len(unmatched))
         for i in range(len(guess)):
             if unmatched[i] == guess[i]:
                 gw.set_square_color(row,i,CORRECT_COLOR)
-                #gw.set_key_color(unmatched[i],CORRECT_COLOR)
+                gw.set_key_color(guess[i],CORRECT_COLOR)
                 #unmatched = remaining_letters(i,answer)
                 unmatched = unmatched[:i] + "-" + unmatched[i+1:]
                 print(unmatched)
@@ -47,14 +58,19 @@ def wordle():
                 color = gw.get_square_color(row,i)
                 if color != CORRECT_COLOR:
                     gw.set_square_color(row,i,PRESENT_COLOR)
-                    #gw.set_key_color(unmatched[i],PRESENT_COLOR)
+                    color = gw.get_key_color(guess[i])
+                    if color != CORRECT_COLOR:
+                        gw.set_key_color(guess[i],PRESENT_COLOR)
                     #unmatched = remaining_letters(i,answer)
                     unmatched = unmatched.replace(guess[i],"-",1)
             else:
                 color = gw.get_square_color(row,i)
                 if color == UNKNOWN_COLOR:
                     gw.set_square_color(row,i,MISSING_COLOR)
-                    #gw.set_key_color(unmatched[i],MISSING_COLOR)
+                    colorb = gw.get_key_color(guess[i])
+                    if colorb == UNKNOWN_COLOR:    
+                        gw.set_key_color(guess[i],MISSING_COLOR)
+        set_row(guess)
     
     def remaining_letters(letter:str, answer:str) -> str:
     # remove ONLY THE FIRST case of letter in word
@@ -76,7 +92,7 @@ def wordle():
     gw.add_enter_listener(enter_action)
 
     
-    answer = "glass" #random_fiveletter_word()
+    answer = random_fiveletter_word()
     gw.show_message(answer)
 
 
