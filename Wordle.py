1ed82d792315701b4d1c98130417414d4a51fcac
fksheets <fksheets@willamette.edu>
2024-09-30 20:30:00 -0700

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -1,25 +1,88 @@
 ########################################
-# Name:
-# Collaborators (if any):
-# GenAI Transcript (if any):
-# Estimated time spent (hr):
-# Description of any added extensions:
+# Name: Flannery Sheets
+# Collaborators (if any): Gavin Smith!!!
+# GenAI Transcript (if any): No
+# Estimated time spent (hr): 5
+# Description of any added extensions: No
 ########################################
 
-from WordleGraphics import *  # WordleGWindow, N_ROWS, N_COLS, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR, UNKNOWN_COLOR
-from english import * # ENGLISH_WORDS, is_english_word
+from WordleGraphics import WordleGWindow, N_ROWS, N_COLS, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR, UNKNOWN_COLOR
+from english import ENGLISH_WORDS, is_english_word
 import random
 
 def wordle():
-    # The main function to play the Wordle game.
 
     def enter_action():
-        # What should happen when RETURN/ENTER is pressed.
-        gw.show_message("You need to implement this method")
+        guess = word_from_row(0)
+        row = gw.get_current_row()
+        print(guess)
+        if guess.lower() in ENGLISH_WORDS:
+            gw.show_message('Good Job!')
+            color_row(row,answer,guess)
+        else:
+            gw.show_message('not word in list')
+
+    def word_to_row(row:int, word:str):
+        gw.show_message("words")
+
+    def word_from_row(row:int) -> str:
+        string = gw.get_square_letter(row,0) + gw.get_square_letter(row,1) + gw.get_square_letter(row,2) + gw.get_square_letter(row,3) + gw.get_square_letter(row,4)
+        print(type (string))
+        return string
+    
+    def color_row(row:int,answer:str,guess:str):
+        guess = word_from_row(row).lower()
+        unmatched = answer
+        #unmatched = guess
+        print(len(unmatched))
+        for i in range(len(guess)):
+            if unmatched[i] == guess[i]:
+                gw.set_square_color(row,i,CORRECT_COLOR)
+                #gw.set_key_color(unmatched[i],CORRECT_COLOR)
+                #unmatched = remaining_letters(i,answer)
+                unmatched = unmatched[:i] + "-" + unmatched[i+1:]
+                print(unmatched)
+        for i in range(len(guess)):    
+            if guess[i] in unmatched:
+                color = gw.get_square_color(row,i)
+                if color != CORRECT_COLOR:
+                    gw.set_square_color(row,i,PRESENT_COLOR)
+                    #gw.set_key_color(unmatched[i],PRESENT_COLOR)
+                    #unmatched = remaining_letters(i,answer)
+                    unmatched = unmatched.replace(guess[i],"-",1)
+            else:
+                color = gw.get_square_color(row,i)
+                if color == UNKNOWN_COLOR:
+                    gw.set_square_color(row,i,MISSING_COLOR)
+                    #gw.set_key_color(unmatched[i],MISSING_COLOR)
+    
+    def remaining_letters(letter:str, answer:str) -> str:
+    # remove ONLY THE FIRST case of letter in word
+        for index in range(len(answer)):
+            if answer[index] == letter:
+                return answer[:index] + '-' + answer[index+1:]
+        return answer # if letter isn't present
+    
+    def random_fiveletter_word() -> str: 
+        random.shuffle(ENGLISH_WORDS)
+        for word in ENGLISH_WORDS:
+            if len(word) == 5:
+                return word
+
 
 
     gw = WordleGWindow()
+
     gw.add_enter_listener(enter_action)
+
+    
+    answer = "glass" #random_fiveletter_word()
+    gw.show_message(answer)
+
+
+
+
+
 
 
 
